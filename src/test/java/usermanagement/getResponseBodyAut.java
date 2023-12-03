347521b434cf0e9e7cd9a74d5dc9f588e4365ece
rayush1309@gmail.com <rayush1309@gmail.com>
2023-12-03 18:43:05 +0530

    Committing changes to AutomateGetResponseBodyAccessToken.java

---
+++ src/test/java/usermanagement/getResponseBodyAut.java
@@ -0,0 +1,21 @@
+package usermanagement;
+import io.restassured.RestAssured;
+import org.testng.annotations.Test;
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.*;
+
+public class getResponseBodyAut {
+    @Test
+    public void getResponseBody() {
+        // Set base URI for the API
+        RestAssured.baseURI = "https://stageapp.tekioncloud.xyz";
+
+        given().when().get("api/wms/parts/u/inventory/utility/extended/report/compute/all-sites")
+                .then()
+                .statusCode(401)
+                .body(not(isEmptyString()))
+                .assertThat()
+                .body("message", equalTo("Token doesn't exist or is invalid"))
+                .body("status", equalTo(401));
+    }
+}
