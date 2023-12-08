21e1d291396d596aef9a8d4555026eaa46ba474d
rayush1309@gmail.com <rayush1309@gmail.com>
2023-12-09 00:00:20 +0530

    Json Array Reader update

--- src/test/java/restAssuredApiFramewok/ValidateWithSoftwareUtils.java
+++ src/test/java/restAssuredApiFramewok/ValidateWithSoftwareUtils.java
@@ -8,14 +8,16 @@ import org.json.simple.JSONArray;
 import org.json.simple.parser.ParseException;
 import org.testng.annotations.Test;
 import utils.JsonArrayReader;
+import utils.JsonReader;
 import utils.PropertyReader;
 import utils.SoftAssertionUtils;
 
 import java.io.IOException;
+import java.util.Iterator;
 
 import static io.restassured.RestAssured.given;
 
-public class ValidateWithSoftwareUtils {
+public class ValidateWithSoftwareUtils extends JsonArrayReader{
     SoftAssertionUtils st= new SoftAssertionUtils();
 
 
@@ -45,6 +47,18 @@ public class ValidateWithSoftwareUtils {
     public void TestJsonArray() throws IOException, ParseException{
         Object ans= JsonArrayReader.getJsonArrayData("languages",0);
         System.out.println(ans);
+    }
+    @Test
+    public void Test() throws IOException,ParseException{
+        JsonArrayReader.getJsonArrayData("technology",2);
+        JSONArray jsonArray= getJsonArray("contact");
+        Iterator<String> It=jsonArray.iterator();
+        while(It.hasNext()){
+            System.out.println(It.next());
+
+
+        }
+
     }
 
 }
