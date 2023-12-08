9ccff7f811ddfc1a049ae33db340123c5ed9a0a3
rayush1309@gmail.com <rayush1309@gmail.com>
2023-12-08 19:38:06 +0530

    Json Array Reader File Update

--- src/main/java/utils/JsonArrayReader.java
+++ src/main/java/utils/JsonArrayReader.java
@@ -1,2 +1,23 @@
-package utils;public class JsonArrayReader {
+package utils;
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.ParseException;
+
+import java.io.IOException;
+
+public class JsonArrayReader extends JsonReader{
+
+    public static JSONArray getJsonArray(String key) throws IOException, ParseException {
+        JSONObject jsonObject=getJsonData();
+        JSONArray jsonArray=(JSONArray) jsonObject.get(key);
+        return  jsonArray;
+
+    }
+    public static Object getJsonArrayData(String key ,int index) throws IOException, ParseException{
+        JSONArray languages=getJsonArray("languages");
+        return languages.get(index);
+
+
+    }
 }
--- src/test/java/restAssuredApiFramewok/ValidateWithSoftwareUtils.java
+++ src/test/java/restAssuredApiFramewok/ValidateWithSoftwareUtils.java
@@ -3,9 +3,15 @@ package restAssuredApiFramewok;
 import core.StatusCode;
 import io.restassured.RestAssured;
 import io.restassured.response.Response;
+import org.apache.groovy.json.internal.IO;
+import org.json.simple.JSONArray;
+import org.json.simple.parser.ParseException;
 import org.testng.annotations.Test;
+import utils.JsonArrayReader;
 import utils.PropertyReader;
 import utils.SoftAssertionUtils;
+
+import java.io.IOException;
 
 import static io.restassured.RestAssured.given;
 
@@ -34,6 +40,11 @@ public class ValidateWithSoftwareUtils {
 
 
 
+    }
+    @Test
+    public void TestJsonArray() throws IOException, ParseException{
+        Object ans= JsonArrayReader.getJsonArrayData("languages",0);
+        System.out.println(ans);
     }
 
 }
