fa9bb6181aa1ca5621cf3f2ce85a7529238a73f2
rayush1309@gmail.com <rayush1309@gmail.com>
2023-12-10 22:39:38 +0530

    Functional Programming

---
+++ src/main/java/coreJava/FunctionalProgramming.java
@@ -0,0 +1,13 @@
+package coreJava;
+
+import org.testng.annotations.Test;
+
+import java.util.function.Function;
+
+public class FunctionalProgramming {
+    @Test
+    public void printSqrt(){
+        Function<Integer,Integer> square=(x)->x*x;
+        System.out.println(square.apply(5));
+    }
+}
